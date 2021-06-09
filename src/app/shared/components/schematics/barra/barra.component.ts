b839769f2ba489d9a8eceb36a09a34382e086aab
vargasjuanj <juanjovargas443377@gmail.com>
2021-06-09 17:09:34 -0300

    Schematics

---
+++ src/app/shared/components/schematics/barra/barra.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-barra',
+  templateUrl: './barra.component.html',
+  styleUrls: ['./barra.component.css']
+})
+export class BarraComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
